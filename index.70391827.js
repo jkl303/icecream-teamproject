ba5bd5ee67f651818112cf5a45375eda1c3333ec
jkl303 <103362175+jkl303@users.noreply.github.com>
2022-07-23 18:20:38 +0000

    Deploying to gh-pages from @ jkl303/goit-team13-icecreamv2@78f82c8c9ba6724150ee7702fb80cdc631c876c8 🚀

---
+++ index.70391827.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".close-menu-btn"),menu:document.querySelector(".mob-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.70391827.js.map
---
+++ index.75c06472.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector(".buy-now"),closeModalBtn:document.querySelector(".close-modal-btn"),modal:document.querySelector(".modal"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.75c06472.js.map
