1323759641b750a161b7361149a464ede02b42f6
jkl303 <103362175+jkl303@users.noreply.github.com>
2022-07-24 09:21:50 +0000

    Deploying to gh-pages from @ jkl303/goit-team13-icecreamv2@0a26ee95b76d7c09cb71eb37dadef14b2bbf9e51 🚀

---
+++ index.892db9dd.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-butto]"),t=document.querySelector("[data-men]");e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.892db9dd.js.map
