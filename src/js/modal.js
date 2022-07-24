9c127bb249be467ecf3f9958649237b8b3a7c028
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-07-24 18:06:13 +0300

    Modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,7 +10,7 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('invisible');
     // refs.body.classList.toggle('no-scroll');
   }
 })();
