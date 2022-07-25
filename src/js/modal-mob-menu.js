ba62e41b83802fab215051a58875f17bbe8e1f92
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-07-25 12:55:57 +0300

    header almost completed
    Added tablet and desktop version.
    Also added one more script

---
+++ src/js/modal-mob-menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.buy-now-menu'),
+    closeModalBtn: document.querySelector('.close-modal-menu'),
+    modal: document.querySelector('.backdrop__header'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('invisible');
+    // refs.body.classList.toggle('no-scroll');
+  }
+})();
