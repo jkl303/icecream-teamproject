6be75546e4bd279dbc76a6aba655453bbf7896c6
jkl303 <103362175+jkl303@users.noreply.github.com>
2022-07-26 08:55:31 +0000

    Deploying to gh-pages from @ jkl303/goit-team13-icecreamv2@7dc226f7970f98dcdd21926c1c276fa28ff78318 🚀

---
+++ index.c3022113.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[hero-button-menu]"),t=document.querySelector("[hero-menu]");e.addEventListener("click",(()=>{const n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.c3022113.js.map
