6f9f62859ac55d5363ce1e83351219e558683a55
jkl303 <103362175+jkl303@users.noreply.github.com>
2022-07-28 10:26:43 +0000

    Deploying to gh-pages from @ jkl303/goit-team13-icecreamv2@e45d5031f2fc97604307487b592c4ec169a795b4 🚀

--- index.4d93d82e.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".slider",{navigarion:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,speed:1e3},centeredSlides:!0,effect:"fade",fadeEffect:{crossFade:!0,speed:1e3,loop:!0},grabCursor:!0});
-//# sourceMappingURL=index.4d93d82e.js.map
