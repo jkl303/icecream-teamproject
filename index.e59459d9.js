5664522170392a5a7bc12bb1e6085e271e336b52
jkl303 <103362175+jkl303@users.noreply.github.com>
2022-07-28 08:54:32 +0000

    Deploying to gh-pages from @ jkl303/goit-team13-icecreamv2@1734a78bce6620dbd70182a0b5cb1e5dee560f4a 🚀

---
+++ index.e59459d9.js
@@ -0,0 +1,2 @@
+document.querySelector(".header__burger-open").addEventListener("click",(function(){var e=document.getElementById("burger");e.classList.contains("hidden")?e.classList.remove("hidden"):e.classList.add("hidden")})),document.querySelector(".header__burger-close").addEventListener("click",(function(){var e=document.getElementById("burger");e.classList.contains("hidden")?e.classList.remove("hidden"):e.classList.add("hidden")}));
+//# sourceMappingURL=index.e59459d9.js.map
---
+++ index.f380a718.js
@@ -0,0 +1,2 @@
+document.querySelector(".header__burger-open").addEventListener("click",(function(){var e=document.getElementById("buy-now-tab");e.classList.contains("hidden")?e.classList.remove("hidden"):e.classList.add("hidden")})),document.querySelector(".header__burger-close").addEventListener("click",(function(){var e=document.getElementById("buy-now-tab");e.classList.contains("hidden")?e.classList.remove("hidden"):e.classList.add("hidden")}));
+//# sourceMappingURL=index.f380a718.js.map
